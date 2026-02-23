ca7978bf661539a0a0b3334e6e8b2ecab4b32894
Ghalia <sayedahmadghalia2@gmail.com>
2026-02-23 22:28:52 +0200

    Fetch about stats from API

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -202,6 +202,7 @@ export default function AboutPage() {
           </div>
         </div>
 
+        {/* Stats Section */}
         {statsError ? (
           <div className="rounded-xl border border-red-200 bg-red-50 px-4 py-3 text-sm text-red-700">
             Failed to load statistics: {statsError}
